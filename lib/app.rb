1f369fadebe4beac503ec7f11e5f2b3176d09991
Jamie Moreland <jamiemorelanduk@gmail.com>
2016-05-27 11:37:16 +0100

    Calculates product and brand details

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,11 @@
 require 'json'
+require 'date'
 path = File.join(File.dirname(__FILE__), '../data/products.json')
 file = File.read(path)
 products_hash = JSON.parse(file)
 
 # Print today's date
+puts Date.today
 
 puts "                     _            _       "
 puts "                    | |          | |      "
@@ -23,6 +25,35 @@ puts "|_|                                       "
   # Calculate and print the average price the toy sold for
   # Calculate and print the average discount (% or $) based off the average sales price
 
+  #Printing the items and prices
+  products_hash["items"].each do |toy|
+      full_price = toy["full-price"]
+      puts "Product name: #{toy["title"]} "
+      puts "Product price: $#{full_price} "
+
+      #Variables set to 0 before loop
+      sale_total = 0
+      purchases = 0
+
+      #Loop to add up sales and purchases
+      toy["purchases"].each do |sale|
+          purchases = purchases + 1
+          sale_total = sale_total + sale["price"] 
+      end
+
+      #Finding average price
+      average_price = sale_total/purchases
+      #Finding average discount
+      full_price = full_price.to_f  #Must be float to divide by another float 
+      average_discount = (average_price / full_price) * 10
+     
+      puts "Total purchases #{purchases} "
+      puts "Total sales $#{sale_total} "
+      puts "Average price $#{average_price} "
+      puts "Average discount: #{average_discount.round(2)}% \n "  
+  end
+
+
 
 	puts " _                         _     "
 	puts "| |                       | |    "
@@ -37,3 +68,46 @@ puts "|_|                                       "
   # Count and print the number of the brand's toys we stock
   # Calculate and print the average price of the brand's toys
   # Calculate and print the total revenue of all the brand's toy sales combined
+
+        brands_hash = Hash.new
+        #Loop to get all values for each brand
+        products_hash["items"].each do |brand|
+            brand_name = brand["brand"]
+            brand_stock = brand["stock"]
+            brand_price = brand["full-price"] 
+            brand_revenue = 0
+        
+            #Loop to add value of all purchases
+            brand["purchases"].each do |purchase|
+                brand_revenue = brand_revenue + purchase["price"]
+            end
+
+            
+
+            #Unless already added to hash add it otherwise update it
+            unless brands_hash.has_key?(brand_name)
+                id = 1
+                brands_hash[brand_name] = {
+                    product_id: id.to_i,
+                    toy_count: brand_stock.to_i, 
+                    avg_price: brand_price.to_f,
+                    revenue: brand_revenue.to_f
+                }
+            else
+                #Updating hash
+                current_brand = brands_hash[brand_name]
+
+                current_brand[:product_id] += 1
+                current_brand[:toy_count] += brand_stock.to_i
+                current_brand[:avg_price] += brand_price.to_f
+                current_brand[:revenue] += brand_revenue.to_f
+            end
+        end
+        #Printing variables
+        brands_hash.each do |brand, rest| 
+            puts "Brand: #{brand} "
+            brand_no = rest[:product_id].to_f
+            puts "Toy Stock: #{rest[:toy_count].to_i }"
+            puts "Average Brand Price: $#{(rest[:avg_price] / brand_no).round(2)} "
+            puts "Revenue: $#{rest[:revenue].round(2)} \n \n"
+        end
